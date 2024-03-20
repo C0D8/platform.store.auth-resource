41b1f600e8bc141706959c6a34adc0fa5a28f84a
C0D8 <matheusaguiar362@gmail.com>
2024-03-20 11:39:18 -0300

    .

---
+++ src/main/java/insper/store/auth/Token.java
@@ -0,0 +1,15 @@
+package insper.store.auth;
+
+import lombok.Builder;
+import lombok.experimental.Accessors;
+
+@Builder
+@Accessors(fluent = true, chain = true)
+public record Token (
+
+        String id,
+        String role,
+        String name
+
+) {
+}
