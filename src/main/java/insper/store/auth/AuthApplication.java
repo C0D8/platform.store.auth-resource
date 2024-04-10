4b97b2a8561e38a6f63dab0b378aadb1b0d444e9
c0d8 <matheusaguiar362@gmail.com>
2024-04-10 10:23:30 -0300

    .

--- src/main/java/insper/store/auth/AuthApplication.java
+++ src/main/java/insper/store/auth/AuthApplication.java
@@ -6,7 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@EnableFeignClients(basePackages = "insper.store.account")
+@EnableFeignClients(basePackages = {
+    "insper.store.account"
+})
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AuthApplication {
